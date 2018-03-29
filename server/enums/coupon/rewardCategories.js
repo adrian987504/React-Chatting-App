bfcb264026a979a059d4f8d6741e4890c0517c66
reactionic127 <richard.gill127@gmail.com>
2018-03-29 18:25:55 +0800

    remove coupon

--- server/enums/coupon/rewardCategories.js
+++
@@ -1,8 +0,0 @@
-const couponRewardCategories = [
-  'bonus',
-  'shop more get more',
-  'discount',
-  'amenities'
-];
-
-module.exports = couponRewardCategories;
--- server/enums/index.js
+++ server/enums/index.js
@@ -1,7 +1,5 @@
 const images = require('./images');
-const coupon = require('./coupon');
 
 module.exports = {
-  images,
-  coupon
+  images
 };
