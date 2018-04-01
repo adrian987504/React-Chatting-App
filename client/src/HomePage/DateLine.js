a81b5c0dbc5269e1fa9748ec29e7ffeec6ba44af
reactionic127 <richard.gill127@gmail.com>
2018-04-01 06:46:32 +0200

    add date line

--- client/src/HomePage/DateLine.js
+++ client/src/HomePage/DateLine.js
@@ -3,12 +3,10 @@ var dateFormat = require('dateformat');
 
 class DateLine extends React.Component {
   render() {
-    // Was the message sent by the current user. If so, add a css class
-    const fromMe = this.props.fromMe ? 'from-me' : '';
     const date = dateFormat(new Date(this.props.date), "dddd, mmmm dS, yyyy");
     return (
-      <div className={`message ${fromMe}`}>
-        {date}
+      <div>
+        <p className="text-divider"><span>{date}</span></p>
       </div>
     );
   }
